8cee07db06e19b648c7a3a7dd20d9b1b22960c2e
Dimcho Lakov <dimcho.lakov@doverfs.com>
2020-11-04 23:55:59 +0000

    Add some unit tests for Catalog microservice

---
+++ MyOnlineShop.Catalog.Tests/CategoriesControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyOnlineShop.Catalog.Controllers;
+using MyOnlineShop.Catalog.Data.Models.Categories;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyOnlineShop.Catalog.Tests
+{
+    public class CategoriesControllerTests : BaseTesting
+    {
+        private readonly CategoriesController categoriesController;
+
+        public CategoriesControllerTests()
+        {
+            this.categoriesController = new CategoriesController(this.CatalogDbContext);
+        }
+
+        [Fact]
+        public async Task StatusChangeShouldChangeStatusOfACategory()
+        {
+            // Arrange
+            int categoryId = 1;
+
+            await this.CatalogDbContext
+                .Categories
+                .AddAsync(new Category
+                {
+                    Id = categoryId,
+                    IsActive = true,
+                    Name = "Test"
+                });
+
+            await this.CatalogDbContext
+                .SaveChangesAsync();
+
+            // Act
+            var actionResult = await this.categoriesController.StatusChange(categoryId);
+
+            // Assert
+            var category = await this.CatalogDbContext
+                .Categories
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+            
+            bool expectedStatus = false;
+            bool actualStatus = category.IsActive;
+
+            Assert.Equal(expectedStatus, actualStatus);
+        }
+    }
+}
